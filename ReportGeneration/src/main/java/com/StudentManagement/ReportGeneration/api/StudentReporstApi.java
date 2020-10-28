ac5ae92659fc3e193f166ee87f2800a0e3338222
Reem <reemhesham51@gmail.com>
2020-10-28 09:49:34 +0200

    c
    editDescription

--- ReportGeneration/src/main/java/com/StudentManagement/ReportGeneration/api/StudentReporstApi.java
+++ ReportGeneration/src/main/java/com/StudentManagement/ReportGeneration/api/StudentReporstApi.java
@@ -29,7 +29,7 @@ public interface StudentReporstApi {
 	/**
 	 * generete report for student
 	 * 
-	 * @param StudentId
+	 * @param reportId
 	 * @return StudentReportDto
 	 */
 	@GetMapping("/{id}")
